1a3cb7f12b9aa057c1c16d74bb08057897e13f2b
Ashik Mridha <ashikmridha43@gmail.com>
2026-04-15 17:24:30 +0600

    remove comment from detailPage

--- src/app/friend/[id]/page.jsx
+++ src/app/friend/[id]/page.jsx
@@ -30,10 +30,10 @@ const FriendDetailPage = async ({ params }) => {
 
         return (
             <div className='md:w-8/12 flex flex-col md:flex-row gap-6 p-6 mx-auto shadow-lg rounded-lg mt-10'>
-                {/* left side */}
+            
                 <div>
                     <div className='text-center lg:max-w-md mx-auto p-5 mr-10'>
-                        {/* left content */}
+                        
                         <div>
                             <img src={friend.picture} alt={friend.name} className='w-20 h-20 rounded-full mx-auto' />
                             <h2 className='text-lg font-semibold mt-2'>{friend.name}</h2>
@@ -46,7 +46,7 @@ const FriendDetailPage = async ({ params }) => {
                             <p className='text-gray-700 mt-3 font-semibold italic'>"{friend.note}"</p>
                             <p className='text-gray-700'>Preferred: {friend.preferred_contact}</p>
                         </div>
-                        {/* actions */}
+                        
                         <div className='mt-6'>
                             <button className='w-full btn bg-gray-100 py-2 rounded'>
                                 Snooze 2 weeks
@@ -61,10 +61,10 @@ const FriendDetailPage = async ({ params }) => {
                     </div>
                 </div>
 
-                {/* right side */}
+
                 <div className='md:col-span-2 space-y-6 p-4 w-full mx-auto'>
-                    {/* states */}
-                    <div className='flex flex-col md:flex-row justify-between space-y-4  text-center gap-4'>
+                    
+                    <div className='flex flex-col md:flex-row justify-between space-y-4 md:w-3/4 mx-auto text-center gap-4'>
                         <div className='bg-white p-4 shadow rounded text-center'>
                             <h3 className='font-bold text-3xl'>{friend.days_since_contact}</h3>
                             <p className='text-gray-600'>Days Since Contact</p>
@@ -80,7 +80,8 @@ const FriendDetailPage = async ({ params }) => {
                             <p className='text-gray-600'>Next Due Date</p>
                         </div>
                     </div>
-                    {/* relationship goal */}
+
+
                     <div className='bg-white p-4 items-center'>
                         <div>
                             <div className='mb-1 flex justify-between items-center'>
@@ -89,11 +90,11 @@ const FriendDetailPage = async ({ params }) => {
                             </div>
                             <p className='text-gray-600'>Connect every <b>{friend.goal}</b> days</p>
                         </div>
-                        <div className='mt-6 shadow-lg border-t border-gray-200 pt-8'>
+                        <div className='mt-6 border-t border-gray-200 pt-8'>
                                 <h3 className='font-semibold text-[#244D3F]'>Quick Check-In</h3>
 
                             <div className='mt-2 flex justify-between text-center items-center md:w-1/2 mx-auto'>
-                                <div className='p-3 shadow'>
+                                <div className='p-3 shadow-sm'>
                                     <button className='bg-gray-200 px-5 py-2 rounded hover:bg-gray-300'>
                                         <Image src={Call} alt="call" className='w-6 h-6 inline-block mr-2' />
                                         Call
--- src/app/timeline/page.jsx
+++ src/app/timeline/page.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const TimelinePage = () => {
     return (
         <div>
-            <h1 className='font-bold text-5xl'>this is Timeline</h1>
+            
         </div>
     );
 };
